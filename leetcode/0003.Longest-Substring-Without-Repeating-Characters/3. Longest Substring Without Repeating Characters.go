c596edd02bd2b71f19689c5e86e2254665f23e24
Frank Wang <yf.frank.wang@outlook.com>
2021-12-04 20:01:37 +0800

    0003.Longest-Substring-Without-Repeating-Characters

---
+++ leetcode/0003.Longest-Substring-Without-Repeating-Characters/3. Longest Substring Without Repeating Characters.go
@@ -0,0 +1,105 @@
+package leetcode
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) < 2 {
+		return len(s)
+	}
+
+	max := 0
+	start := 0
+	end := start + 1
+	for ; end < len(s); end++ {
+		b := s[end]
+
+		// find duplicate
+		d := -1
+		for n := start; n < end; n++ {
+			if s[n] == b {
+				d = n
+				break
+			}
+		}
+		if d >= 0 {
+			l := end - start
+			if l > max {
+				max = l
+			}
+			start = d + 1
+		}
+	}
+
+	l := end - start
+	if l > max {
+		max = l
+	}
+	return max
+}
+
+// 解法一 位图
+func lengthOfLongestSubstring0(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	var bitSet [256]bool
+	result, left, right := 0, 0, 0
+	for left < len(s) {
+		// 右侧字符对应的 bitSet 被标记 true，说明此字符在 X 位置重复，需要左侧向前移动，直到将 X 标记为 false
+		if bitSet[s[right]] {
+			bitSet[s[left]] = false
+			left++
+		} else {
+			bitSet[s[right]] = true
+			right++
+		}
+		if result < right-left {
+			result = right - left
+		}
+		if left+result >= len(s) || right >= len(s) {
+			break
+		}
+	}
+	return result
+}
+
+// 解法二 滑动窗口
+func lengthOfLongestSubstring1(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	var freq [256]int
+	result, left, right := 0, 0, -1
+
+	for left < len(s) {
+		if right+1 < len(s) && freq[s[right+1]-'a'] == 0 {
+			freq[s[right+1]-'a']++
+			right++
+		} else {
+			freq[s[left]-'a']--
+			left++
+		}
+		result = max(result, right-left+1)
+	}
+	return result
+}
+
+// 解法三 滑动窗口-哈希桶
+func lengthOfLongestSubstring2(s string) int {
+	right, left, res := 0, 0, 0
+	indexes := make(map[byte]int, len(s))
+	for left < len(s) {
+		if idx, ok := indexes[s[left]]; ok && idx >= right {
+			right = idx + 1
+		}
+		indexes[s[left]] = left
+		left++
+		res = max(res, left-right)
+	}
+	return res
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
